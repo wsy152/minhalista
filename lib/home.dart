213707850d47542323619594d6d9a79c614b0e56
Edvaldo Santana <edvaldosantana@Mac-mini-de-Edvaldo.local>
2020-12-16 22:21:08 -0300

    Atualização

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,7 @@
+import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -10,67 +12,94 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final _toDoController = TextEditingController();
+
   List _toDoList = [];
-  void _addTodo() {
-    Map<String, dynamic> newTodo = Map();
-    setState(() {
-      newTodo['title'] = _toDoController.text;
-      _toDoController.text = '';
-      newTodo['ok'] = false;
-      _toDoList.add(newTodo);
+
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _readData().then((data) {
+      setState(() {
+        _toDoList = json.decode(data);
+      });
     });
+  }
+
+  void _addToDo() {
+    setState(() {
+      Map<String, dynamic> newToDo = Map();
+      newToDo["title"] = _toDoController.text;
+      _toDoController.text = "";
+      newToDo["ok"] = false;
+      _toDoList.add(newToDo);
+
+      _saveData();
+    });
+  }
+
+  Future<Null> _refresh() async {
+    await Future.delayed(Duration(seconds: 1));
+
+    setState(() {
+      _toDoList.sort((a, b) {
+        if (a["ok"] && !b["ok"])
+          return 1;
+        else if (!a["ok"] && b["ok"])
+          return -1;
+        else
+          return 0;
+      });
+
+      _saveData();
+    });
+
+    return null;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Minha lista de tarefas'),
-        backgroundColor: Colors.blue,
+        title: Text("Lista de Tarefas"),
+        backgroundColor: Colors.blueAccent,
         centerTitle: true,
       ),
       body: Column(
-        children: [
+        children: <Widget>[
           Container(
-            padding: EdgeInsets.fromLTRB(17, 1, 7, 1),
+            padding: EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
             child: Row(
-              children: [
+              children: <Widget>[
                 Expanded(
-                  child: TextField(
-                    controller: _toDoController,
-                    decoration: InputDecoration(
-                        labelText: 'Nova tarefa',
-                        labelStyle: TextStyle(color: Colors.blueAccent)),
+                    child: TextField(
+                  controller: _toDoController,
+                  decoration: InputDecoration(
+                    labelText: "Nova Tarefa",
+                    labelStyle: TextStyle(
+                      color: Colors.blueAccent,
+                    ),
                   ),
-                ),
+                )),
                 RaisedButton(
                   color: Colors.blueAccent,
-                  child: Text('add'),
-                  onPressed: _addTodo,
+                  child: Text("ADD"),
                   textColor: Colors.white,
+                  onPressed: _addToDo,
                 )
               ],
             ),
           ),
           Expanded(
-            child: ListView.builder(
-              padding: EdgeInsets.only(top: 10),
-              itemCount: _toDoList.length,
-              itemBuilder: (context, index) {
-                return CheckboxListTile(
-                  title: Text(_toDoList[index]['title']),
-                  value: _toDoList[index][''],
-                  secondary: CircleAvatar(
-                    child: Icon(
-                        _toDoList[index]['ok'] ? Icons.check : Icons.error),
-                  ),
-                  onChanged: (c) {
-                    setState(() {
-                      _toDoList[index]['ok'] = c;
-                    });
-                  },
-                );
-              },
+            child: RefreshIndicator(
+              onRefresh: _refresh,
+              child: ListView.builder(
+                  padding: EdgeInsets.only(top: 10.0),
+                  itemCount: _toDoList.length,
+                  itemBuilder: buildItem),
             ),
           )
         ],
@@ -78,13 +107,71 @@ class _HomeState extends State<Home> {
     );
   }
 
+  Widget buildItem(BuildContext context, int index) {
+    return Dismissible(
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      background: Container(
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment(-0.9, 0.0),
+          child: Icon(
+            Icons.delete,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+        title: Text(_toDoList[index]["title"]),
+        value: _toDoList[index]["ok"],
+        secondary: CircleAvatar(
+          child: Icon(_toDoList[index]["ok"] ? Icons.check : Icons.error),
+        ),
+        onChanged: (c) {
+          setState(() {
+            _toDoList[index]["ok"] = c;
+            _saveData();
+          });
+        },
+      ),
+      onDismissed: (direction) {
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
+
+          _saveData();
+
+          final snack = SnackBar(
+            content: Text("Tarefa \"${_lastRemoved["title"]}\" removida!"),
+            action: SnackBarAction(
+                label: "Desfazer",
+                onPressed: () {
+                  setState(() {
+                    _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                    _saveData();
+                  });
+                }),
+            duration: Duration(seconds: 2),
+          );
+
+          // ignore: deprecated_member_use
+          Scaffold.of(context).removeCurrentSnackBar();
+          // ignore: deprecated_member_use
+          Scaffold.of(context).showSnackBar(snack);
+        });
+      },
+    );
+  }
+
   Future<File> _getFile() async {
-    final diretorio = await getApplicationDocumentsDirectory();
-    return File('${diretorio.path}/data.json');
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
   }
 
   Future<File> _saveData() async {
     String data = json.encode(_toDoList);
+
     final file = await _getFile();
     return file.writeAsString(data);
   }
@@ -92,6 +179,7 @@ class _HomeState extends State<Home> {
   Future<String> _readData() async {
     try {
       final file = await _getFile();
+
       return file.readAsString();
     } catch (e) {
       return null;
